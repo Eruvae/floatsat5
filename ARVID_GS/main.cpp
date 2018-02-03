d1fc5ace1efd882a3ad994b4ccc0da781336e3ee
spacepirate7 <harshaxd@gmail.com>
2018-02-03 22:31:45 +0100

    Added Splash Screen

--- ARVID_GS/main.cpp
+++ ARVID_GS/main.cpp
@@ -1,11 +1,22 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSplashScreen>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QSplashScreen *splash=new QSplashScreen;
+    splash->setPixmap(QPixmap(":/Project Images/Img/ARVID.jpg"));
+    splash->show();
+
     MainWindow w;
-    w.show();
+
+    QTimer::singleShot(2500,splash,SLOT(close()));
+    QTimer::singleShot(2500,&w,SLOT(show()));
+ //   w.show();
 
     return a.exec();
 }
+
