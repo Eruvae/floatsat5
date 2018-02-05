f68d0943478b5de61420bdbc1f3b1f68f3688df6
Eruvae <tobi.zaenker@web.de>
2018-02-05 16:12:49 +0100

    Added possibility to turn of RW-Speed-Control

--- floatsat5-obc/PoseController.cpp
+++ floatsat5-obc/PoseController.cpp
@@ -17,31 +17,121 @@ PoseController::PoseController() : filterePoseSub(itFilteredPose, filteredPoseBu
 								//otDataSub(itObjectTrackingPose, otDataBuffer)
 {}
 
+void PoseController::controlYaw()
+{
+	// yaw control
+	float yaw = filteredPose.yaw;
+	float goalYaw = targetPose.yaw; // TODO: change via Topic
+
+	//const float p = 15.0f;
+	//const float i = 0.1f;
+	//const float d = 0.2f;
+
+	float error = goalYaw - yaw;
+	MOD(error, -180, 180);
+
+	errYaw_int += error * period;
+
+	float rwSpeedDifDps = attP * error + attD * filteredPose.dyaw + attI * errYaw_int;
+
+	int16_t rwSpeedDifRpm = (int16_t)(rwSpeedDifDps / 6);
+	int16_t newRwSpeed = currentRwSpeed + rwSpeedDifRpm;
+
+	tcReactionWheelTargetSpeed.put(newRwSpeed);
+}
+
+void PoseController::controlPosition()
+{
+	// position control
+	float x = filteredPose.x;
+	float y = filteredPose.y;
+	float goalX = targetPose.x;
+	float goalY = targetPose.y;
+
+	// USE OT YAW
+	/*OTData otData;
+	otDataBuffer.get(otData);
+	float r = otData.g0 + 0.2;
+	float otYaw = M_PI + otData.alpha + atan2(otData.G0, r);*/
+	// END
+
+	float eX = goalX - x;
+	float eY = goalY - y;
+	rotateCoord(eX, eY, filteredPose.yaw*M_PI/180.f , eX, eY);
+
+	float eX_dif = (eX - oldeX) / period;
+	float eY_dif = (eY - oldeY) / period;
+
+	eX_int += eX * period;
+	eY_int += eY * period;
+
+	oldeX = eX;
+	oldeY = eY;
+
+	float zX = k * (eX + td * eX_dif + ti*eX_int);
+	float zY = k * (eY + td * eY_dif + ti*eY_int);
+
+	float gX = SIGN(zX) * (1.f - exp(-ABS(zX))) / sqrt(2);
+	float gY = SIGN(zY) * (1.f - exp(-ABS(zY))) / sqrt(2);
+
+	float alpha = atan2(gY, gX) * 180.f/M_PI;
+	MOD(alpha, 0.f, 360.f);
+
+	//print_debug_msg("Gx: %.2f, Gy: %.2f, Yaw: %.2f, a: %.2f", gX, gY, filteredPose.yaw, alpha);
+
+	ThrusterControls controls;
+	if (alpha > 300 || alpha < 60)
+	{
+		controls.f1 = 0; // gamma*gX + gamma*gY/sqrt(3);
+		controls.f2 = gamma*(gX - gY/sqrt(3));//2*gamma/sqrt(3)*gY;
+		controls.f3 = gamma*(gX + gY/sqrt(3));//0;
+	}
+	else if (alpha < 180)
+	{
+		controls.f1 = gamma*(-gX + gY/sqrt(3));//0;
+		controls.f2 = 0; //gamma*gY/sqrt(3) - gamma*gX;
+		controls.f3 = 2.f/sqrt(3)*gamma*gY; //-gamma*gX - gamma*gY/sqrt(3);
+	}
+	else
+	{
+		controls.f1 = gamma*(-gX - gY/sqrt(3));//gamma*gX - gamma*gY/sqrt(3);
+		controls.f2 = -2.f/sqrt(3)*gamma*gY;
+		controls.f3 = 0; //-2*gamma*gY/sqrt(3);
+	}
+
+	//float dummyX = 1, dummyY = 0;
+	//rotateCoord(dummyX, dummyY, M_PI/2.f, dummyX, dummyY);
+	//print_debug_msg("Dx: %.2f, Dy: %.2f\n", dummyX, dummyY);
+
+	//print_debug_msg("a: %.2f, f1: %.2f, f2: %.2f, f3: %.2f\n", alpha, controls.f1, controls.f2, controls.f3);
+	itThrusterControls.publish(controls);
+}
 
 void PoseController::run()
 {
-	const float period = 0.2;
+
 	setPeriodicBeat(20*MILLISECONDS, period*SECONDS);
-	Pose filteredPose;
+	/*Pose filteredPose;
 	Pose2D targetPose = {0};
 	//tcTargetPose.put(targetPose); // Start with 0
 	int16_t currentRwSpeed;
 	//bool activated = false;
-	PoseControllerMode mode = PoseControllerMode::STANDBY;
+	PoseControllerMode mode = PoseControllerMode::STANDBY;*/
 	//tcActivateController.put(activated);
 	itPoseControllerMode.publish(mode);
-	float oldeX = 0, oldeY = 0;
+	/*float oldeX = 0, oldeY = 0;
 	float eX_int = 0, eY_int = 0;
 	float oldYawErr = 0;
 	float errYaw_int = 0;
 	float errRot_int = 0, old_dYaw = 0;
 	float attP = 10.f, attD = -4.f, attI = 0.f;
 	float rotP = 100.f, rotD = 0.f, rotI = 10.f;
-	float k = 1.5f, td = 5.f, ti = 0.05f, gamma = sqrt(3)/2; // trajectory control params
+	float k = 1.5f, td = 5.f, ti = 0.05f, gamma = sqrt(3)/2; // trajectory control params*/
 	ControlParameters params = {attP, attD, attI, k, td, ti, rotP, rotD, rotI};
 	tcControlParams.put(params);
 	bool valvePWMEnabled = false;
 	activateValvePWM.put(valvePWMEnabled);
+	bool activateRWSpeedControl = true;
 	while(1)
 	{
 		//tcActivateController.get(activated);
@@ -50,11 +140,33 @@ void PoseController::run()
 		filteredPoseBuffer.get(filteredPose);
 		reactionWheelSpeedBuffer.get(currentRwSpeed);
 		Pose2D oldTargetPose = targetPose;
+		if (mode != PoseControllerMode::FOLLOW_TRAJECTORY && !tcNextTargetPoseList.isEmpty())
+		{
+			mode = PoseControllerMode::FOLLOW_TRAJECTORY;
+			itPoseControllerMode.publish(mode);
+			tcNextTargetPoseList.get(targetPose);
+			targetPoseSemaphore.enter();
+			tcTargetPose.put(targetPose);
+			targetPoseSemaphore.leave();
+		}
+
 		tcTargetPose.get(targetPose);
+
 		tcControlParams.get(params);
 		attP = params.attP; attD = params.attD; attI = params.attI;
 		k = params.traP; td = params.traD; ti = params.traI;
 		rotP = params.rotP; rotD = params.rotD; rotI = params.rotI;
+
+		/*if (activateRWSpeedControl && ABS(currentRwSpeed) > 4500)
+		{
+			activateRWSpeedControl = false;
+			activateRWSpeedController.put(activateRWSpeedControl);
+		}
+		else if (!activateRWSpeedControl && ABS(currentRwSpeed) < 2000)
+		{
+			activateRWSpeedControl = true;
+			activateRWSpeedController.put(activateRWSpeedControl);
+		}*/
 
 		if (targetPose.x != oldTargetPose.x ||
 			targetPose.y != oldTargetPose.y ||
@@ -69,25 +181,9 @@ void PoseController::run()
 			valvePWMEnabled = false;
 			activateValvePWM.put(valvePWMEnabled);
 
-			// yaw control
-			float yaw = filteredPose.yaw;
-			float goalYaw = targetPose.yaw; // TODO: change via Topic
+			//if (activateRWSpeedControl)
+			controlYaw();
 
-			//const float p = 10.0f;
-			//const float i = 0.1f;
-			//const float d = 0.5f;
-
-			float error = goalYaw - yaw;
-			MOD(error, -180, 180);
-
-			errYaw_int += error * period;
-
-			float rwSpeedDifDps = attP * error + attD * filteredPose.dyaw + attI * errYaw_int;
-
-			int16_t rwSpeedDifRpm = (int16_t)(rwSpeedDifDps / 6);
-			int16_t newRwSpeed = currentRwSpeed + rwSpeedDifRpm;
-
-			tcReactionWheelTargetSpeed.put(newRwSpeed);
 		}
 		else if (mode == PoseControllerMode::ROTATE)
 		{
@@ -111,99 +207,39 @@ void PoseController::run()
 
 			tcReactionWheelTargetSpeed.put(newRwSpeed);
 		}
-		else if (mode == PoseControllerMode::FOLLOW_TRAJECTORY)
+		else if (mode == PoseControllerMode::GOTO_POSE)
 		{
 			// activate valve PWM
 			valvePWMEnabled = true;
 			activateValvePWM.put(valvePWMEnabled);
 
-			// yaw control
-			float yaw = filteredPose.yaw;
-			float goalYaw = targetPose.yaw; // TODO: change via Topic
+			//if (activateRWSpeedControl)
+			controlYaw();
 
-			//const float p = 15.0f;
-			//const float i = 0.1f;
-			//const float d = 0.2f;
-
-			float error = goalYaw - yaw;
-			MOD(error, -180, 180);
-
-			errYaw_int += error * period;
-
-			float rwSpeedDifDps = attP * error + attD * filteredPose.dyaw + attI * errYaw_int;
-
-			int16_t rwSpeedDifRpm = (int16_t)(rwSpeedDifDps / 6);
-			int16_t newRwSpeed = currentRwSpeed + rwSpeedDifRpm;
-
-			tcReactionWheelTargetSpeed.put(newRwSpeed);
-
-			// position control
-			float x = filteredPose.x;
-			float y = filteredPose.y;
-			float goalX = targetPose.x;
-			float goalY = targetPose.y;
-
-			// USE OT YAW
-			/*OTData otData;
-			otDataBuffer.get(otData);
-			float r = otData.g0 + 0.2;
-			float otYaw = M_PI + otData.alpha + atan2(otData.G0, r);*/
-			// END
-
-			float eX = goalX - x;
-			float eY = goalY - y;
-			rotateCoord(eX, eY, filteredPose.yaw*M_PI/180.f , eX, eY);
-
-			float eX_dif = (eX - oldeX) / period;
-			float eY_dif = (eY - oldeY) / period;
-
-			eX_int += eX * period;
-			eY_int += eY * period;
-
-			oldeX = eX;
-			oldeY = eY;
-
-			float zX = k * (eX + td * eX_dif + ti*eX_int);
-			float zY = k * (eY + td * eY_dif + ti*eY_int);
-
-			float gX = SIGN(zX) * (1.f - exp(-ABS(zX))) / sqrt(2);
-			float gY = SIGN(zY) * (1.f - exp(-ABS(zY))) / sqrt(2);
-
-			float alpha = atan2(gY, gX) * 180.f/M_PI;
-			MOD(alpha, 0.f, 360.f);
-
-			//print_debug_msg("Gx: %.2f, Gy: %.2f, Yaw: %.2f, a: %.2f", gX, gY, filteredPose.yaw, alpha);
-
-			ThrusterControls controls;
-			if (alpha > 300 || alpha < 60)
-			{
-				controls.f1 = 0; // gamma*gX + gamma*gY/sqrt(3);
-				controls.f2 = gamma*(gX - gY/sqrt(3));//2*gamma/sqrt(3)*gY;
-				controls.f3 = gamma*(gX + gY/sqrt(3));//0;
-			}
-			else if (alpha < 180)
-			{
-				controls.f1 = gamma*(-gX + gY/sqrt(3));//0;
-				controls.f2 = 0; //gamma*gY/sqrt(3) - gamma*gX;
-				controls.f3 = 2.f/sqrt(3)*gamma*gY; //-gamma*gX - gamma*gY/sqrt(3);
-			}
-			else
-			{
-				controls.f1 = gamma*(-gX - gY/sqrt(3));//gamma*gX - gamma*gY/sqrt(3);
-				controls.f2 = -2.f/sqrt(3)*gamma*gY;
-				controls.f3 = 0; //-2*gamma*gY/sqrt(3);
-			}
-
-			//float dummyX = 1, dummyY = 0;
-			//rotateCoord(dummyX, dummyY, M_PI/2.f, dummyX, dummyY);
-			//print_debug_msg("Dx: %.2f, Dy: %.2f\n", dummyX, dummyY);
-
-			//print_debug_msg("a: %.2f, f1: %.2f, f2: %.2f, f3: %.2f\n", alpha, controls.f1, controls.f2, controls.f3);
-			itThrusterControls.publish(controls);
+			controlPosition();
 		}
-		else if (mode == PoseControllerMode::MOMENTUM_DAMPING)
+		else if (mode == PoseControllerMode::FOLLOW_TRAJECTORY)
 		{
+			valvePWMEnabled = true;
+			activateValvePWM.put(valvePWMEnabled);
 
+			if (ABS(targetPose.x - filteredPose.x) < 0.1 && ABS(targetPose.y - filteredPose.y) < 0.1)
+			{
+				if (!tcNextTargetPoseList.isEmpty())
+				{
+					tcNextTargetPoseList.get(targetPose);
+					targetPoseSemaphore.enter();
+					tcTargetPose.put(targetPose);
+					targetPoseSemaphore.leave();
+				}
+				else
+				{
+					mode = PoseControllerMode::GOTO_POSE;
+				}
+			}
+
+			controlYaw();
+			controlPosition();
 		}
 		else
 		{
